412ffc1896a96115f41b30890d0b5181e3ecd066
Peter Dinis <petter1@pobx.sk>
2026-05-09 14:58:39 +0200

    infra: Optimalize next.config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
 	experimental: {
 		typedEnv: true,
 	},
+	 images: {
+        formats: ['image/webp'],
+        deviceSizes: [640, 750, 828, 1080, 1200],
+        imageSizes: [16, 32, 64, 96, 128, 256],
+        minimumCacheTTL: 300,
+    },
 };
 
 export default nextConfig;
